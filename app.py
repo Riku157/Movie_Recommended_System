db03ce8723c80fbc76674bbeb966678a98137329
Riku157 <61966240+Riku157@users.noreply.github.com>
2025-03-29 17:14:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,45 +11,42 @@ gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentic
 
 drive = GoogleDrive(gauth)
 
-# Download the file
+# Download the simi.pkl file from Google Drive
 file_id = '1sA855TxW06kVm-PISKG2zQOamy_4qmUO'
 file = drive.CreateFile({'id': file_id})
 file.GetContentFile('simi.pkl')
 
-
-
+# Function to fetch movie poster
 def fetch_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=1f5930a5b48827077a2d420afa7fa447'.format(movie_id))
+    response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=1f5930a5b48827077a2d420afa7fa447')
     data = response.json()
-    print(data)
     return "https://image.tmdb.org/t/p/w500" + data['poster_path']
 
-
+# Function to recommend movies
 def recommend_movies(movie):
     movies_index = movies[movies['title'] == movie].index[0]
     distance = simi[movies_index]
     movies_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]
 
-
-    recommend_movies=[]
-    poster=[]
+    recommend_movies = []
+    poster = []
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
         recommend_movies.append(movies.iloc[i[0]].title)
         poster.append(fetch_poster(movie_id))
     return recommend_movies, poster
 
-
+# Load movie list and similarity data
 movies_list = pkl.load(open('movie_list.pkl', 'rb'))
 movies = pd.DataFrame(movies_list)
 simi = pkl.load(open('simi.pkl', 'rb'))
 
+# Streamlit UI
 st.title("Movie Recommendation System")
 
-
-option = st.selectbox('Name your Movie', movies_list['title'].values)
+option = st.selectbox('Choose a Movie', movies['title'].values)
 if st.button('Recommend Movie'):
-    name,posters=recommend_movies(option)
+    name, posters = recommend_movies(option)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(name[0])
