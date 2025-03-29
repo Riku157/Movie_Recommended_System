e1be6aaec5d74b22470f02c2e5b33d9faf4513c8
Riku157 <61966240+Riku157@users.noreply.github.com>
2025-03-29 17:19:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,34 +7,47 @@ from pydrive.drive import GoogleDrive
 
 # Authenticate
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
+gauth.LocalWebserverAuth()  # Creates a local web server for authentication
 
 drive = GoogleDrive(gauth)
 
-# Download the simi.pkl file from Google Drive
-file_id = '1sA855TxW06kVm-PISKG2zQOamy_4qmUO'
-file = drive.CreateFile({'id': file_id})
-file.GetContentFile('simi.pkl')
+# Download the simi.pkl and movie_list.pkl files from Google Drive
+def download_file(file_id, file_name):
+    file = drive.CreateFile({'id': file_id})
+    file.GetContentFile(file_name)
+
+# Download the movie list and similarity matrix files
+download_file('1sA855TxW06kVm-PISKG2zQOamy_4qmUO', 'simi.pkl')
+download_file('movie_list_file_id', 'movie_list.pkl')  # Replace 'movie_list_file_id' with actual ID
 
 # Function to fetch movie poster
 def fetch_poster(movie_id):
-    response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=1f5930a5b48827077a2d420afa7fa447')
-    data = response.json()
-    return "https://image.tmdb.org/t/p/w500" + data['poster_path']
+    try:
+        response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=1f5930a5b48827077a2d420afa7fa447')
+        data = response.json()
+        poster_url = "https://image.tmdb.org/t/p/w500" + data['poster_path']
+        return poster_url
+    except Exception as e:
+        print(f"Error fetching poster: {e}")
+        return "https://image.tmdb.org/t/p/w500/default_image.jpg"  # Placeholder image if there's an error
 
 # Function to recommend movies
 def recommend_movies(movie):
-    movies_index = movies[movies['title'] == movie].index[0]
-    distance = simi[movies_index]
-    movies_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]
+    try:
+        movies_index = movies[movies['title'] == movie].index[0]
+        distance = simi[movies_index]
+        movies_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]  # Top 5 recommendations
 
-    recommend_movies = []
-    poster = []
-    for i in movies_list:
-        movie_id = movies.iloc[i[0]].movie_id
-        recommend_movies.append(movies.iloc[i[0]].title)
-        poster.append(fetch_poster(movie_id))
-    return recommend_movies, poster
+        recommend_movies = []
+        poster = []
+        for i in movies_list:
+            movie_id = movies.iloc[i[0]].movie_id
+            recommend_movies.append(movies.iloc[i[0]].title)
+            poster.append(fetch_poster(movie_id))
+        return recommend_movies, poster
+    except Exception as e:
+        print(f"Error in recommending movies: {e}")
+        return [], []
 
 # Load movie list and similarity data
 movies_list = pkl.load(open('movie_list.pkl', 'rb'))
@@ -47,19 +60,11 @@ st.title("Movie Recommendation System")
 option = st.selectbox('Choose a Movie', movies['title'].values)
 if st.button('Recommend Movie'):
     name, posters = recommend_movies(option)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(name[0])
-        st.image(posters[0])
-    with col2:
-        st.text(name[1])
-        st.image(posters[1])
-    with col3:
-        st.text(name[2])
-        st.image(posters[2])
-    with col4:
-        st.text(name[3])
-        st.image(posters[3])
-    with col5:
-        st.text(name[4])
-        st.image(posters[4])
+    if name and posters:
+        col1, col2, col3, col4, col5 = st.columns(5)
+        for i, col in enumerate([col1, col2, col3, col4, col5]):
+            with col:
+                st.text(name[i])
+                st.image(posters[i])
+    else:
+        st.error("Sorry, no recommendations available.")
